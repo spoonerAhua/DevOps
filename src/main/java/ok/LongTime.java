fb2b6040c2a8eda307147e288c144bc68c47f461
spooner <xwhxuxwh@cn.ibm.com>
2018-08-31 20:28:21 +0800

    test

---
+++ src/main/java/ok/LongTime.java
@@ -0,0 +1,12 @@
+package ok;
+
+public class LongTime {
+
+    public static void main(String[] args) {
+    }
+    
+    public static int say(){
+        return 1000;
+    }
+
+}
---
+++ src/main/java/ok/TestLongTime.java
@@ -0,0 +1,29 @@
+package ok;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestLongTime extends LongTime {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {}
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {}
+
+    @Before
+    public void setUp() throws Exception {}
+
+    @After
+    public void tearDown() throws Exception {}
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1000, LongTime.say());
+    }
+
+}
